ff204e8f7ab4187c831d5e2fc59dcc45419bd50a
pholdahl <pal.holdahl@gmail.com>
2022-09-01 11:00:26 +0200

    Initial commit

---
+++ src/Uke34.java
@@ -0,0 +1,12 @@
+public class Uke34 {
+
+    public static void main(String[] args) {
+        System.out.println("Hei AlgDat!");
+
+        System.out.println("Teller til ti");
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+    }
+
+}
